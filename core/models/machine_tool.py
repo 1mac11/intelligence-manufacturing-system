850935e94ded69f8a1cc3c1d0cfec4998e907339
1mac11 <bekturdiyevzarif@gmail.com>
2022-06-14 17:50:33 +0500

    fix MachineTool type

--- core/models/machine_tool.py
+++ core/models/machine_tool.py
@@ -5,7 +5,7 @@ from core.models import BaseModel
 
 class MachineTool(BaseModel):
     name = models.CharField(max_length=255, unique=True)
-    type = models.ForeignKey('core.MachineToolType', on_delete=models.SET_NULL, related_name='machine_tools',
+    type = models.ForeignKey('core.MachineToolType', on_delete=models.CASCADE, related_name='machine_tools',
                              blank=True, null=True)
     status = models.ForeignKey('core.Status', on_delete=models.SET_NULL, related_name='machine_tools', blank=True,
                                null=True)
