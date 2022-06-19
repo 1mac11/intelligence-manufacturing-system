ab9286f2f96f7b905cb90db211333b2c6ede675b
1mac11 <bekturdiyevzarif@gmail.com>
2022-06-19 17:08:32 +0500

    create team validators

--- api/serializers/team.py
+++ api/serializers/team.py
@@ -1,21 +1,25 @@
 from rest_framework import serializers
 
 from api.serializers import BaseModelSerializer
-from core.models import Team, User
+from core.models import Team, User, Status
+from core.models.user_type import UserTypeChoices
+from core.utils.checkers import check_team
 
 
 class TeamSerializer(BaseModelSerializer):
     users = serializers.ListSerializer(child=serializers.PrimaryKeyRelatedField(queryset=User.objects.all()))
-    status_id = serializers.IntegerField(write_only=True)
     status = serializers.CharField(source='status.name', read_only=True)
 
     class Meta(BaseModelSerializer.Meta):
         model = Team
-        fields = BaseModelSerializer.Meta.fields + ('name', 'users', 'status_id', 'status',
-                                                    'machine_tool', 'building')
+        fields = BaseModelSerializer.Meta.fields + ('name', 'users', 'status', 'machine_tool', 'building')
+        extra_fields = {'status': {'read_only': True}}
 
     def create(self, validated_data):
         users = validated_data.pop('users')
+        managers = User.objects.filter(id__in=(user.id for user in users), detail__type__name=UserTypeChoices.MANAGER)
+        workers = User.objects.filter(id__in=(user.id for user in users), detail__type__name=UserTypeChoices.WORKER)
+        validated_data['status'] = Status.objects.get(name=check_team(managers=managers, workers=workers))
 
         team = super().create(validated_data)
         for user in users:
@@ -24,7 +28,10 @@ class TeamSerializer(BaseModelSerializer):
         return team
 
     def update(self, instance, validated_data):
-        validated_data.pop('users')
-        instance = super().update(instance, validated_data)
+        users = validated_data.pop('users')
+        managers = User.objects.filter(id__in=(user.id for user in users), detail__type__name=UserTypeChoices.MANAGER)
+        workers = User.objects.filter(id__in=(user.id for user in users), detail__type__name=UserTypeChoices.WORKER)
+        validated_data['status'] = Status.objects.get(name=check_team(managers=managers, workers=workers, team=instance))
 
+        instance = super().update(instance, validated_data)
         return instance
--- core/utils/checkers.py
+++ core/utils/checkers.py
@@ -1,6 +1,60 @@
-def check_perm():
-    return True
+from typing import Optional
+
+from rest_framework.exceptions import ValidationError
+from django.db.models import Q
+
+from core.models import Team
+from core.models.status import StatusChoices
+from core.models.user_type import UserTypeChoices
 
 
-def team_checker():
-    return True
+def check_managers(managers, team=None):
+    if team is None:
+        print(managers)
+        for manager in managers:
+            if manager.teams.exists():
+                raise ValidationError({'manager': "Manager shouldn't be on other teams"})
+    else:
+        if managers.exclude(Q(teams__isnull=True) | Q(teams=team)).exists():
+            raise ValidationError({'manager': "Manager shouldn't be on other teams"})
+
+
+def check_workers(workers, team=None):
+    if team is None:
+        for worker in workers:
+            if worker.teams.exists():
+                raise ValidationError({'worker': "Worker shouldn't be on other teams"})
+    else:
+        if workers.exclude(Q(teams__isnull=True) | Q(teams=team)).exists():
+            raise ValidationError({'worker': "Worker shouldn't be on other teams"})
+
+
+def check_size(workers):
+    if workers.count() > 5:
+        raise ValidationError({'size': "Team should be consist of no more than 5 workers."})
+
+
+def update_team_check(team: Team, workers, managers):
+    workers = team.users.filter(detail__type__name=UserTypeChoices.WORKER) | workers
+    managers = team.users.filter(detail__type__name=UserTypeChoices.MANAGER) | managers
+    check_managers(managers, team)
+    check_workers(workers, team)
+    check_size(workers)
+    if workers.count() == 0 or managers.count() == 0:
+        return StatusChoices.PENDING
+    return StatusChoices.WORKING
+
+
+def create_team_check(workers, managers):
+    check_managers(managers)
+    check_workers(workers)
+    check_size(workers)
+    if workers.count() == 0 or managers.count() == 0:
+        return StatusChoices.PENDING
+    return StatusChoices.WORKING
+
+
+def check_team(managers, workers, team: Optional[Team] = None):
+    if team:
+        return update_team_check(team, managers, workers)
+    return create_team_check(managers, workers)
